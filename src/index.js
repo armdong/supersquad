a17abcc7593b700c70e5e8bbe7275a34d0f62fd6
armdong <armdong@163.com>
2018-03-23 15:39:32 +0800

    added deploy scripts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
-import { addCharacterById } from './actions'
 
 const store = createStore(rootReducer)
 
